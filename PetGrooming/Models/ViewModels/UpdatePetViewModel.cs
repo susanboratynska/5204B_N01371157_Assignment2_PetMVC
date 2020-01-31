cda38fa77c4cd6659a9c522953e6aba9df5aacac
Susan Boratynska <susan.boratynska@gmail.com>
2020-01-31 10:44:29 -0500

    Added comments.

--- PetGrooming/Models/ViewModels/UpdatePetViewModel.cs
+++ PetGrooming/Models/ViewModels/UpdatePetViewModel.cs
@@ -7,11 +7,12 @@ namespace PetGrooming.Models.ViewModels
 {
     public class UpdatePetViewModel
     {
-        //WHAT INFO DOES UPDATE PET NEED?
-        //NEEDS LIST OF SPECIES AND INDIVIDUAL PET
+        // WHAT INFO DOES UPDATE PET NEED?
+        // IT NEEDS LIST OF SPECIES AND INDIVIDUAL PET
 
-            // Acts as a variable to contain the information that we need
+        // Acts as a variable to contain the information that we need
 
+        // IT NEEDS THE INFO OF A PET AND THE LIST OF SPECIES TO POPULATE THE DROP DOWN LIST:
         public Pet pet { get; set; }
         public List<Species> species { get; set; }
     }
